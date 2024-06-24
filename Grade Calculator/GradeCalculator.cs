27e1fc6e44f3c1e21cd9bef9a90116df7aeb92fd
Isaac Wilson <iswilson.dev@gmail.com>
2024-06-24 19:39:06 +0000

    feat: Add grade calculator task

---
+++ Grade Calculator/GradeCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+
+namespace Grade_Calculator;
+
+public class GradeCalculator
+{
+   public static double TakeInput()
+   {
+      double invalidScore = -1;
+
+      Console.WriteLine("Enter your score:");
+
+      string? input = Console.ReadLine();
+
+      if (string.IsNullOrWhiteSpace(input))
+      {
+         Console.WriteLine("Please enter a valid number");
+         return invalidScore;
+      }
+
+      bool validInput = double.TryParse(input, NumberStyles.Number, CultureInfo.InvariantCulture, out double result);
+
+      if (validInput)
+      {
+         if (result < 0 || result > 100)
+         {
+            Console.WriteLine("Score cannot be less than 0 or greater than 100");
+            return invalidScore;
+         }
+      }
+      else
+      {
+         Console.WriteLine("Enter a valid number");
+         return invalidScore;
+      }
+
+      return result;
+   }
+
+
+   public static string CalculateGrade(double score)
+   {
+
+      if (score == -1)
+      {
+         return "Invalid Score";
+      }
+
+      string grade = "";
+      switch (score)
+      {
+         case >= 90:
+            grade = "A";
+            break;
+         case >= 80:
+            grade = "B";
+            break;
+         case >= 70:
+            grade = "C";
+            break;
+         case >= 60:
+            grade = "D";
+            break;
+
+         default:
+            grade = "F";
+            break;
+      }
+
+      return $"Your grade is: {grade}";
+   }
+}
---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grade_Calculator;
+
+// Take score from user
+double score = GradeCalculator.TakeInput();
+
+// Calculate grade of score
+string grade = GradeCalculator.CalculateGrade(score);
+
+// Display Grade information
+Console.WriteLine(grade);
